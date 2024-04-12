a1c8ab931f31c47619c37abbdab9abb647fd6744
emir14t <emir360t@gmail.com>
2024-04-12 13:36:32 -0400

    server CRUD is clean

--- server/app/controllers/database.controller.ts
+++ server/app/controllers/database.controller.ts
@@ -3,12 +3,13 @@ import { inject, injectable } from "inversify";
 import * as pg from "pg";
 
 import { Hotel } from "../../../common/tables/Hotel";
-import { HotelPK } from "../../../common/tables/HotelPK";
+import { Bird } from "../../../common/tables/Bird";
 import { Room } from "../../../common/tables/Room";
 import { Guest } from "../../../common/tables/Guest";
 
 import { DatabaseService } from "../services/database.service";
 import Types from "../types";
+import console = require("console");
 
 @injectable()
 export class DatabaseController {
@@ -19,244 +20,66 @@ export class DatabaseController {
   public get router(): Router {
     const router: Router = Router();
 
-    // ======= HOTEL ROUTES =======
-    // ex http://localhost:3000/database/hotel?hotelNb=3&name=LeGrandHotel&city=laval
-    router.get("/hotels", (req: Request, res: Response, _: NextFunction) => {
-      var hotelNb = req.params.hotelNb ? req.params.hotelNb : "";
-      var hotelName = req.params.name ? req.params.name : "";
-      var hotelCity = req.params.city ? req.params.city : "";
-
+    // ======= BIRD ROUTES =======
+    // ex http://localhost:3000/database/oiseau?nomScientifique=smth
+    router.get("/oiseau", (req: Request, res: Response) => {
       this.databaseService
-        .filterHotels(hotelNb, hotelName, hotelCity)
+        .getAllFromTable('especeoiseau')
         .then((result: pg.QueryResult) => {
-          const hotels: Hotel[] = result.rows.map((hotel: Hotel) => ({
-            hotelnb: hotel.hotelnb,
-            name: hotel.name,
-            city: hotel.city,
-          }));
-          res.json(hotels);
+          res.status(200).send(result.rows);
         })
         .catch((e: Error) => {
-          console.error(e.stack);
+          res.status(400).send();
         });
     });
 
-    router.get(
-      "/hotels/hotelNb",
-      (req: Request, res: Response, _: NextFunction) => {
-        this.databaseService
-          .getHotelNamesByNos()
-          .then((result: pg.QueryResult) => {
-            const hotelsNbsNames = result.rows.map((hotel: HotelPK) => ({
-              hotelnb: hotel.hotelnb,
-              name: hotel.name,
-            }));
-            res.json(hotelsNbsNames);
-          })
+    router.post("/oiseau", (req: Request, res: Response) => {
+        const bird: Bird = {
+          nomScientifique: req.body.nomScientifique,
+          nomCommun: req.body.nomCommun,
+          statusEspece: req.body.statusEspece,
+          predateur: req.body.predateur,
+        }
 
-          .catch((e: Error) => {
-            console.error(e.stack);
-          });
-      }
-    );
-
-    router.post(
-      "/hotels/insert",
-      (req: Request, res: Response, _: NextFunction) => {
-        const hotel: Hotel = {
-          hotelnb: req.body.hotelnb,
-          name: req.body.name,
-          city: req.body.city,
-        };
-
-        this.databaseService
-          .createHotel(hotel)
+        this.databaseService.createBird(bird)
           .then((result: pg.QueryResult) => {
             res.json(result.rowCount);
           })
           .catch((e: Error) => {
-            console.error(e.stack);
             res.json(-1);
           });
       }
     );
 
-    router.post(
-      "/hotels/delete/:hotelNb",
-      (req: Request, res: Response, _: NextFunction) => {
-        const hotelNb: string = req.params.hotelNb;
-        this.databaseService
-          .deleteHotel(hotelNb)
-          .then((result: pg.QueryResult) => {
-            res.json(result.rowCount);
-          })
-          .catch((e: Error) => {
-            console.error(e.stack);
-          });
-      }
-    );
-
-    router.put(
-      "/hotels/update",
-      (req: Request, res: Response, _: NextFunction) => {
-        const hotel: Hotel = {
-          hotelnb: req.body.hotelnb,
-          name: req.body.name ? req.body.name : "",
-          city: req.body.city ? req.body.city : "",
-        };
-
-        this.databaseService
-          .updateHotel(hotel)
-          .then((result: pg.QueryResult) => {
-            res.json(result.rowCount);
-          })
-          .catch((e: Error) => {
-            console.error(e.stack);
-          });
-      }
-    );
-
-    // ======= ROOMS ROUTES =======
-    router.get("/rooms", (req: Request, res: Response, _: NextFunction) => {
-      const hotelNb = req.query.hotelNb ? req.query.hotelNb : "";
-      const roomNb = req.query.roomNb ? req.query.roomNb : "";
-      const roomType = req.query.type ? req.query.type : "";
-      const roomPrice = req.query.price
-        ? parseFloat(req.query.price as string)
-        : -1;
-
-      this.databaseService
-        .filterRooms(
-          hotelNb as string,
-          roomNb as string,
-          roomType as string,
-          roomPrice
-        )
+    router.patch('/oiseau/update/:nomScientifique', (req: Request, res: Response) => {
+      const pKey = req.params.nomScientifique;
+      const newBird: Bird = {
+        nomScientifique: req.body.nomScientifique || '',
+        nomCommun: req.body.nomCommun || '',
+        statusEspece: req.body.statusEspece || '',
+        predateur: req.body.predateur
+      };
+      this.databaseService.updateBird(newBird, pKey)
         .then((result: pg.QueryResult) => {
-          const rooms: Room[] = result.rows.map((room: Room) => ({
-            hotelnb: room.hotelnb,
-            roomnb: room.roomnb,
-            type: room.type,
-            price: parseFloat(room.price.toString()),
-          }));
-
-          res.json(rooms);
+          res.status(200).send(':)');
         })
         .catch((e: Error) => {
-          console.error(e.stack);
+          console.log(e);
+          res.status(400).send();
         });
     });
 
-    router.post(
-      "/rooms/insert",
-      (req: Request, res: Response, _: NextFunction) => {
-        const room: Room = {
-          hotelnb: req.body.hotelnb,
-          roomnb: req.body.roomnb,
-          type: req.body.type,
-          price: parseFloat(req.body.price),
-        };
-
-        this.databaseService
-          .createRoom(room)
-          .then((result: pg.QueryResult) => {
-            res.json(result.rowCount);
-          })
-          .catch((e: Error) => {
-            console.error(e.stack);
-            res.json(-1);
-          });
-      }
-    );
-
-    router.put(
-      "/rooms/update",
-      (req: Request, res: Response, _: NextFunction) => {
-        const room: Room = {
-          hotelnb: req.body.hotelnb,
-          roomnb: req.body.roomnb,
-          type: req.body.type,
-          price: parseFloat(req.body.price),
-        };
-
-        this.databaseService
-          .updateRoom(room)
-          .then((result: pg.QueryResult) => {
-            res.json(result.rowCount);
-          })
-          .catch((e: Error) => {
-            console.error(e.stack);
-            res.json(-1);
-          });
-      }
-    );
-
-    router.post(
-      "/rooms/delete/:hotelNb/:roomNb",
-      (req: Request, res: Response, _: NextFunction) => {
-        const hotelNb: string = req.params.hotelNb;
-        const roomNb: string = req.params.roomNb;
-
-        this.databaseService
-          .deleteRoom(hotelNb, roomNb)
-          .then((result: pg.QueryResult) => {
-            res.json(result.rowCount);
-          })
-          .catch((e: Error) => {
-            console.error(e.stack);
-            res.json(-1);
-          });
-      }
-    );
-
-    // ======= GUEST ROUTES =======
-    router.post(
-      "/guests/insert",
-      (req: Request, res: Response, _: NextFunction) => {
-        const guest: Guest = {
-          guestnb: req.body.guestnb,
-          nas: req.body.nas,
-          name: req.body.name,
-          gender: req.body.gender,
-          city: req.body.city,
-        };
-
-        this.databaseService
-          .createGuest(guest)
-          .then((result: pg.QueryResult) => {
-            res.json(result.rowCount);
-          })
-          .catch((e: Error) => {
-            console.error(e.stack);
-            res.json(-1);
-          });
-      }
-    );
-
-    router.get(
-      "/guests/:hotelNb/:roomNb",
-      (req: Request, res: Response, _: NextFunction) => {
-        const hotelNb: string = req.params.hotelNb;
-        const roomNb: string = req.params.roomNb;
-
-        this.databaseService
-          .getGuests(hotelNb, roomNb)
-          .then((result: pg.QueryResult) => {
-            const guests: Guest[] = result.rows.map((guest: any) => ({
-              guestnb: guest.guestnb,
-              nas: guest.nas,
-              name: guest.name,
-              gender: guest.gender,
-              city: guest.city,
-            }));
-            res.json(guests);
-          })
-          .catch((e: Error) => {
-            console.error(e.stack);
-            res.json(-1);
-          });
-      }
-    );
+    router.delete('/oiseau/:nomScientifique', (req: Request, res: Response) => {
+      const pKey = req.params.nomScientifique;
+      this.databaseService.deleteBird(pKey)
+        .then((result: pg.QueryResult) => {
+          res.status(202).send();
+        })
+        .catch((e: Error) => {
+          console.log(e);
+          res.status(400).send();
+        });
+    });
 
     // ======= GENERAL ROUTES =======
     router.get(
--- server/app/services/database.service.ts
+++ server/app/services/database.service.ts
@@ -1,9 +1,9 @@
 import { injectable } from "inversify";
 import * as pg from "pg";
 import "reflect-metadata";
-import { Room } from "../../../common/tables/Room";
-import { Hotel } from "../../../common/tables/Hotel";
-import { Gender, Guest } from "../../../common/tables/Guest";
+import { Bird } from "../../../common/tables/Bird";
+
+const DATABASE_NAME = 'ornithologue_bd';
 
 @injectable()
 export class DatabaseService {
@@ -11,9 +11,9 @@ export class DatabaseService {
   public connectionConfig: pg.ConnectionConfig = {
     user: "postgres",
     database: "postgres",
-    password: "mysecretpassword",
-    port: 5432,
-    host: "127.0.0.1",
+    password: "qwerty",
+    port: 5555,
+    host: "0.0.0.0",
     keepAlive: true,
   };
 
@@ -22,238 +22,66 @@ export class DatabaseService {
   // ======= DEBUG =======
   public async getAllFromTable(tableName: string): Promise<pg.QueryResult> {
     const client = await this.pool.connect();
-    const res = await client.query(`SELECT * FROM HOTELDB.${tableName};`);
+    const res = await client.query(`SELECT * FROM ${DATABASE_NAME}.${tableName};`);
     client.release();
     return res;
   }
 
-  // ======= HOTEL =======
-  public async createHotel(hotel: Hotel): Promise<pg.QueryResult> {
+  // ======= BIRD =======
+  public async createBird(bird: Bird): Promise<pg.QueryResult> {
+    if (!bird.nomCommun || !bird.nomScientifique || !bird.statusEspece) {
+      throw new Error('Invalid create bird');
+    }
     const client = await this.pool.connect();
-
-    if (!hotel.hotelnb || !hotel.name || !hotel.city)
-      throw new Error("Invalid create hotel values");
-
-    const values: string[] = [hotel.hotelnb, hotel.name, hotel.city];
-    const queryText: string = `INSERT INTO HOTELDB.Hotel VALUES($1, $2, $3);`;
+    const values = [bird.nomScientifique, bird.nomCommun, bird.statusEspece, bird.predateur]
+    const queryText = `INSERT INTO ${DATABASE_NAME}.especeoiseau VALUES($1, $2, $3, $4)`
 
     const res = await client.query(queryText, values);
     client.release();
     return res;
   }
 
-  // get hotels that correspond to certain caracteristics
-  public async filterHotels(
-    hotelNb: string,
-    hotelName: string,
-    city: string
-  ): Promise<pg.QueryResult> {
-    const client = await this.pool.connect();
+  public async updateBird(bird: Bird, oldBirdID: string): Promise<pg.QueryResult> {
+      if (!oldBirdID) {
+        throw new Error('ID invalid')
+      }
+      const client = await this.pool.connect();
 
-    const searchTerms: string[] = [];
-    if (hotelNb.length > 0) searchTerms.push(`hotelNb = '${hotelNb}'`);
-    if (hotelName.length > 0) searchTerms.push(`name = '${hotelName}'`);
-    if (city.length > 0) searchTerms.push(`city = '${city}'`);
+      const queryText = `SELECT * FROM ${DATABASE_NAME}.especeoiseau WHERE nomscientifique = '${oldBirdID}';`;
+      const res = await client.query(queryText);
+      if (res.rows.length === 0) {
+        throw new Error(`NAME ${oldBirdID} does not exist in collection`)
+      }
 
-    let queryText = "SELECT * FROM HOTELDB.Hotel";
-    if (searchTerms.length > 0)
-      queryText += " WHERE " + searchTerms.join(" AND ");
-    queryText += ";";
+      const paramsToUpdate: string[] = [];
 
-    const res = await client.query(queryText);
-    client.release();
-    return res;
+      if (bird.nomScientifique.length > 0) { paramsToUpdate.push(`nomscientifique = '${ bird.nomScientifique }'`); }
+      if (bird.nomCommun.length > 0) { paramsToUpdate.push(`nomcommun = '${ bird.nomCommun }'`); }
+      if (bird.statusEspece.length > 0) { paramsToUpdate.push(`statutspeces = '${ bird.statusEspece }'`); }
+      if (bird.predateur && bird.predateur.length > 0) { paramsToUpdate.push(`nomscientifiquecomsommer = '${ bird.predateur }'`); }
+
+      const updateQueryText = `UPDATE ${DATABASE_NAME}.especeoiseau SET ${paramsToUpdate.join(", ")} WHERE nomscientifique = '${oldBirdID}'`;
+      const updateRes = await client.query(updateQueryText);
+
+      client.release();
+      return updateRes;
   }
 
-  // get the hotel names and numbers so so that the user can only select an existing hotel
-  public async getHotelNamesByNos(): Promise<pg.QueryResult> {
-    const client = await this.pool.connect();
-    const res = await client.query("SELECT hotelNb, name FROM HOTELDB.Hotel;");
-    client.release();
-    return res;
-  }
-
-  // modify name or city of a hotel
-  public async updateHotel(hotel: Hotel): Promise<pg.QueryResult> {
-    const client = await this.pool.connect();
-
-    let toUpdateValues = [];
-
-    if (hotel.name.length > 0) toUpdateValues.push(`name = '${hotel.name}'`);
-    if (hotel.city.length > 0) toUpdateValues.push(`city = '${hotel.city}'`);
-
-    if (
-      !hotel.hotelnb ||
-      hotel.hotelnb.length === 0 ||
-      toUpdateValues.length === 0
-    )
-      throw new Error("Invalid hotel update query");
-
-    const query = `UPDATE HOTELDB.Hotel SET ${toUpdateValues.join(
-      ", "
-    )} WHERE hotelNb = '${hotel.hotelnb}';`;
-    const res = await client.query(query);
-    client.release();
-    return res;
-  }
-
-  public async deleteHotel(hotelNb: string): Promise<pg.QueryResult> {
-    if (hotelNb.length === 0) throw new Error("Invalid delete query");
+  public async deleteBird(birdID: string): Promise<pg.QueryResult> {
+    if (birdID.length === 0) throw new Error("Invalid delete query");
 
     const client = await this.pool.connect();
-    const query = `DELETE FROM HOTELDB.Hotel WHERE hotelNb = '${hotelNb}';`;
 
-    const res = await client.query(query);
-    client.release();
-    return res;
-  }
-
-  // ======= ROOMS =======
-  public async createRoom(room: Room): Promise<pg.QueryResult> {
-    const client = await this.pool.connect();
-
-    if (!room.roomnb || !room.hotelnb || !room.type || !room.price)
-      throw new Error("Invalid create room values");
-
-    const values: string[] = [
-      room.roomnb,
-      room.hotelnb,
-      room.type,
-      room.price.toString(),
-    ];
-    const queryText: string = `INSERT INTO HOTELDB.ROOM VALUES($1, $2, $3, $4);`;
-
-    const res = await client.query(queryText, values);
-    client.release();
-    return res;
-  }
-
-  public async filterRooms(
-    hotelNb: string,
-    roomNb: string = "",
-    roomType: string = "",
-    price: number = -1
-  ): Promise<pg.QueryResult> {
-    const client = await this.pool.connect();
-
-    if (!hotelNb || hotelNb.length === 0)
-      throw new Error("Invalid filterRooms request");
-
-    let searchTerms = [];
-    searchTerms.push(`hotelNb = '${hotelNb}'`);
-
-    if (roomNb.length > 0) searchTerms.push(`hotelNb = '${hotelNb}'`);
-    if (roomType.length > 0) searchTerms.push(`type = '${roomType}'`);
-    if (price >= 0) searchTerms.push(`price = ${price}`);
-
-    let queryText = `SELECT * FROM HOTELDB.Room WHERE ${searchTerms.join(
-      " AND "
-    )};`;
-    const res = await client.query(queryText);
-    client.release();
-    return res;
-  }
-
-  public async updateRoom(room: Room): Promise<pg.QueryResult> {
-    const client = await this.pool.connect();
-
-    let toUpdateValues = [];
-    if (room.price >= 0) toUpdateValues.push(`price = ${room.price}`);
-    if (room.type.length > 0) toUpdateValues.push(`type = '${room.type}'`);
-
-    if (
-      !room.hotelnb ||
-      room.hotelnb.length === 0 ||
-      !room.roomnb ||
-      room.roomnb.length === 0 ||
-      toUpdateValues.length === 0
-    )
-      throw new Error("Invalid room update query");
-
-    const query = `UPDATE HOTELDB.Room SET ${toUpdateValues.join(
-      ", "
-    )} WHERE hotelNb = '${room.hotelnb}' AND roomNb = '${room.roomnb}';`;
-    const res = await client.query(query);
-    client.release();
-    return res;
-  }
-
-  public async deleteRoom(
-    hotelNb: string,
-    roomNb: string
-  ): Promise<pg.QueryResult> {
-    if (hotelNb.length === 0) throw new Error("Invalid room delete query");
-    const client = await this.pool.connect();
-
-    const query = `DELETE FROM HOTELDB.Room WHERE hotelNb = '${hotelNb}' AND roomNb = '${roomNb}';`;
-    const res = await client.query(query);
-    client.release();
-    return res;
-  }
-
-  // ======= GUEST =======
-  public async createGuest(guest: Guest): Promise<pg.QueryResult> {
-    const client = await this.pool.connect();
-    if (
-      !guest.guestnb ||
-      !guest.nas ||
-      !guest.name ||
-      !guest.gender ||
-      !guest.city
-    )
-      throw new Error("Invalid create room values");
-
-    if (!(guest.gender in Gender))
-      throw new Error("Unknown guest gender passed");
-
-    const values: string[] = [
-      guest.guestnb,
-      guest.nas,
-      guest.name,
-      guest.gender,
-      guest.city,
-    ];
-    const queryText: string = `INSERT INTO HOTELDB.Guest VALUES($1, $2, $3, $4, $5);`;
-    const res = await client.query(queryText, values);
-    client.release();
-    return res;
-  }
-
-  public async getGuests(
-    hotelNb: string,
-    roomNb: string
-  ): Promise<pg.QueryResult> {
-    if (!hotelNb || hotelNb.length === 0)
-      throw new Error("Invalid guest hotel no");
-
-    const client = await this.pool.connect();
-    const queryExtension = roomNb ? ` AND b.roomNb = '${roomNb}'` : "";
-    const query: string = `SELECT * FROM HOTELDB.Guest g JOIN HOTELDB.Booking b ON b.guestNb = g.guestNb WHERE b.hotelNb = '${hotelNb}'${queryExtension};`;
-
-    const res = await client.query(query);
-    client.release();
-    return res;
-  }
-
-  // ======= BOOKING =======
-  public async createBooking(
-    hotelNb: string,
-    guestNo: string,
-    dateFrom: Date,
-    dateTo: Date,
-    roomNb: string
-  ): Promise<pg.QueryResult> {
-    const client = await this.pool.connect();
-    const values: string[] = [
-      hotelNb,
-      guestNo,
-      dateFrom.toString(),
-      dateTo.toString(),
-      roomNb,
-    ];
-    const queryText: string = `INSERT INTO HOTELDB.ROOM VALUES($1,$2,$3,$4,$5);`;
-
-    const res = await client.query(queryText, values);
+    /* dereference foreign key in db before deleting */
+    const updateQuery = `UPDATE ${DATABASE_NAME}.especeoiseau 
+    SET nomscientifiquecomsommer = NULL 
+    WHERE nomscientifiquecomsommer = '${birdID}';`;
+    await client.query(updateQuery);
+    
+    /* delete the entry */
+    const deleteQuery = `DELETE FROM ${DATABASE_NAME}.especeoiseau WHERE nomscientifique = '${birdID}';`;
+    const res = await client.query(deleteQuery);
+    
     client.release();
     return res;
   }
